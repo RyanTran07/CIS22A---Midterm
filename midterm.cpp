4ce7a93911c840f684ec3f7116fcc27344f19eee
RyanTran07 <rtran934@gmail.com>
2025-05-25 00:13:50 -0700

    Made some final changes

--- midterm.cpp
+++ midterm.cpp
@@ -37,12 +37,12 @@ int main() {
 
     // User input loop for burger selection.
     while(!orderEnded) {
-        cout << "Please choose a burger option, or select 6 to end order: ";
+        cout << "\nPlease choose a burger option, or select 6 to end order: ";
         cin >> burgerSelection;
 
         // Input validation for the option selection.
         while(burgerSelection < 1 || burgerSelection > 6) {
-            cout << "Please choose a valid burger option: ";
+            cout << "Please choose a valid option (1-6): ";
             cin >> burgerSelection;
         }
 
@@ -56,7 +56,7 @@ int main() {
         else {
 
             // User input selection for the number of burgers selected
-            cout << "How many burgers would you like? : ";
+            cout << "\nHow many burgers would you like? : ";
             cin >> userQuantity;
 
             // Input validation, only allows non-negative input for # of burgers
@@ -89,7 +89,7 @@ int main() {
 
     // Customer type input selection
     int userCustomerType;
-    cout << "Are you a student or staff?" << endl;
+    cout << "\nAre you a student or staff?" << endl;
     cout << "1. Student\n" << "2. Staff\n";
     cin.ignore();
     cin >> userCustomerType;
@@ -103,7 +103,8 @@ int main() {
 
 
     // Displaying the quantity of each burger that is ordered.
-    cout << "\nDe Anza Burger Quantity Ordered: " << quantity1 << endl;
+    cout << "\nOrdered Items and Quantities:" << endl;
+    cout << "De Anza Burger Quantity Ordered: " << quantity1 << endl;
     cout << "Bacon Cheese Burger Quantity Ordered: " << quantity2 << endl;
     cout << "Mushroom Swiss Burger Quantity Ordered: " << quantity3 << endl;
     cout << "Western Burger Quantity Ordered: " << quantity4 << endl;
@@ -127,19 +128,13 @@ int main() {
     cout << "Western Burger: $5.95 x " << quantity4 << " = $" << costQuantity4 << endl;
     cout << "Don Cali Burger: $5.95 x " << quantity5 << " = $" << costQuantity5 << endl;
 
-    // Displaying the subtotal of each burger (cost of all items before tax)
-    cout << "\nTotal Before Tax: " << endl;
-    cout << "De Anza Burger: $" << costQuantity1 << endl;
-    cout << "Bacon Cheese Burger: $" << costQuantity2 << endl;
-    cout << "Mushroom Swiss Burger: $" << costQuantity3 << endl;
-    cout << "Western Burger Burger: $" << costQuantity4 << endl;
-    cout << "Don Cali Burger: $" << costQuantity5 << endl;
 
     // Calculation for the subTotal (cost of all burgers ordered)
     double subTotal = costQuantity1 + costQuantity2 + costQuantity3 + costQuantity4 + costQuantity5;
 
-    // Displaying the sub total
-    cout << "\nSub Total: $" << subTotal << endl;
+    // Displaying the subtotal of each burger (cost of all items before tax)
+    cout << "\nTotal Before Tax: $" << subTotal << endl;
+
 
     // Declaring and initializing a varialbe to represent tax
     double taxAmount = 0;
@@ -153,7 +148,7 @@ int main() {
     cout << "\nTax Amount: $" << taxAmount << endl;
 
     // Display the final total
-    cout << "\nFinal Bill: $" << (subTotal + taxAmount) << endl;
+    cout << "\nFinal Total: $" << (subTotal + taxAmount) << endl;
 
     // Make an ofstream object and open a new output.txt file
     ofstream outputFile;
@@ -162,9 +157,17 @@ int main() {
     // If file successfully opened, write the monetary value of the final bill
     if(outputFile) {
 
+        outputFile << "Ordered Items and Quantities:" << endl;
+        outputFile << "De Anza Burger Quantity Ordered: " << quantity1 << endl;
+        outputFile << "Bacon Cheese Burger Quantity Ordered: " << quantity2 << endl;
+        outputFile << "Mushroom Swiss Burger Quantity Ordered: " << quantity3 << endl;
+        outputFile << "Western Burger Quantity Ordered: " << quantity4 << endl;
+        outputFile << "Don Cali Burger Quantity Ordered: " << quantity5 << endl;
+        outputFile << endl;
+
         outputFile << fixed << showpoint << setprecision(2);
         // Writing the cost per item
-        outputFile << "\nCost Per Item: " << endl;
+        outputFile << "Cost Per Item: " << endl;
         outputFile << "De Anza Burger: $5.25 x " << quantity1 << " = $" << costQuantity1 << endl;
         outputFile << "Bacon Cheese Burger: $5.75 x " << quantity2 << " = $" << costQuantity2 << endl;
         outputFile << "Mushroom Swiss Burger: $5.95 x " << quantity3 << " = $" << costQuantity3 << endl;
@@ -172,14 +175,7 @@ int main() {
         outputFile << "Don Cali Burger: $5.95 x " << quantity5 << " = $" << costQuantity5 << endl;
 
         // Writing the sub total
-        outputFile << "\nTotal Before Tax: " << endl;
-        outputFile << "De Anza Burger: $" << costQuantity1 << endl;
-        outputFile << "Bacon Cheese Burger: $" << costQuantity2 << endl;
-        outputFile << "Mushroom Swiss Burger: $" << costQuantity3 << endl;
-        outputFile << "Western Burger Burger: $" << costQuantity4 << endl;
-        outputFile << "Don Cali Burger: $" << costQuantity5 << endl;
-
-        outputFile << "\nSub Total: $" << subTotal << endl;
+        outputFile << "\nTotal Before Tax: $" << subTotal << endl;
 
         // Displaying the tax amount
         outputFile << "\nTax Amount: $" << taxAmount << endl;
@@ -196,3 +192,84 @@ int main() {
 
     return 0;
 }
+
+/*
+    Run 1 (including inputs):
+    1. De Anza Burger - $5.25
+    2. Bacon Cheese Burger - $5.75
+    3. Mushroom Swiss Burger - $5.95
+    4. Western Burger - $5.95
+    5. Don Cali Burger - $5.95
+    6. End Order
+
+    Please choose a burger option, or select 6 to end order: 1
+
+    How many burgers would you like? : 10
+
+    Please choose a burger option, or select 6 to end order: 2
+
+    How many burgers would you like? : 20
+
+    Please choose a burger option, or select 6 to end order: 3
+
+    How many burgers would you like? : 30
+
+    Please choose a burger option, or select 6 to end order: 4
+
+    How many burgers would you like? : 40
+
+    Please choose a burger option, or select 6 to end order: 5
+
+    How many burgers would you like? : 50
+
+    Please choose a burger option, or select 6 to end order: 1
+
+    How many burgers would you like? : 5
+
+    Please choose a burger option, or select 6 to end order: 2
+
+    How many burgers would you like? : 4
+
+    Please choose a burger option, or select 6 to end order: 3
+
+    How many burgers would you like? : 3
+
+    Please choose a burger option, or select 6 to end order: 4
+
+    How many burgers would you like? : 2
+
+    Please choose a burger option, or select 6 to end order: 5
+
+    How many burgers would you like? : 1
+
+    Please choose a burger option, or select 6 to end order: 6
+
+    Are you a student or staff?
+    1. Student
+    2. Staff
+    2
+
+    Ordered Items and Quantities:
+    De Anza Burger Quantity Ordered: 15
+    Bacon Cheese Burger Quantity Ordered: 24
+    Mushroom Swiss Burger Quantity Ordered: 33
+    Western Burger Quantity Ordered: 42
+    Don Cali Burger Quantity Ordered: 51
+
+    Cost Per Item:
+    De Anza Burger: $5.25 x 15 = $78.75
+    Bacon Cheese Burger: $5.75 x 24 = $138.00
+    Mushroom Swiss Burger: $5.95 x 33 = $196.35
+    Western Burger: $5.95 x 42 = $249.90
+    Don Cali Burger: $5.95 x 51 = $303.45
+
+    Total Before Tax: $966.45
+
+    Tax Amount: $86.98
+
+    Final Total: $1053.43
+
+
+
+    Run 2:
+*/
